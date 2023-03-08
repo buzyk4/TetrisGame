a9867cec1d53c428f76177134c63f54bf3083d93
ArcadiusBuzyk <buzyka99@gmail.com>
2023-03-08 15:15:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,57 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-btn')
     const width = 10
 
+    //The Block shapes
+    const lBlock = [
+        [1,width+1,width*2+1,2],
+        [width,width+1,width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2]
+    ]
+
+    const tBlock = [
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1]
+    ]
+
+    const zBlock = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+
+    const oBlock = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+
+    const iBlock = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const theBlocks = [lBlock, zBlock, oBlock, iBlock]
+
+    let currentPosition = 4
+    let currentRotation = 0
+
+    //Randomly select block
+    let random = Math.floor(Math.random()*theBlocks.length)
+    let current = theBlocks[random][0]
+
+    //Draw the block
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('block')
+        })
+    }
+
+
 })
