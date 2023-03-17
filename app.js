992f5491f2145db115cd92fb050cdaec086c0419
ArcadiusBuzyk <buzyka99@gmail.com>
2023-03-17 10:35:44 +0100

    final chcnges for tetris music

--- app.js
+++ app.js
@@ -282,19 +282,32 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //music
-function playMusic(videoId) {
-    var player = document.getElementById('my-iframe');
-    player.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
-    document.getElementsByClassName('music').style.display = 'block';
+// global variable for the player
+var player;
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+   // create the global player from the specific iframe (#video)
+   player = new YT.Player('video', {
+      events: {
+         // call this function when player is ready to use
+         'onReady': onPlayerReady
+      }
+   });
 }
 
-function pauseMusic() {
-    var iframe = document.getElementById('my-iframe');
-    var player = new YT.Player(iframe);
-    
-    if (player.getPlayerState() == YT.PlayerState.PLAYING) {
+function onPlayerReady(event) {
+   var playButton = document.getElementById("play-button");
+   playButton.addEventListener("click", function () {
+      player.playVideo();
+   });
+   var pauseButton = document.getElementById("pause-button");
+   pauseButton.addEventListener("click", function () {
       player.pauseVideo();
-    }
+   });
 }
-  
-document.getElementById("pause-btn").addEventListener("click", pauseMusic);
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
